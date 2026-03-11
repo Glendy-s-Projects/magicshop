2e6ab0d02ee60ad0a0f1114a531f765421594507
GlendyT <glendytuyuc@gmail.com>
2026-03-10 18:15:05 -0600

    adding the break line to the song name, and adding another div to properly download the image

--- src/app/arirang/ResultadoCard1.tsx
+++ src/app/arirang/ResultadoCard1.tsx
@@ -14,14 +14,16 @@ const ResultadoCard1 = ({
   const songName = usuario?.song || resultado?.song || "Arirang";
 
   return (
-    <div className="w-full h-full relative">
+    <div 
+      className="w-full h-96 relative"
+      id={isActive ? "print" : undefined}
+    >
       <div
-        className={`${darkSide ? " bg-cardArirangWhite" : "bg-cardArirangRed"} bg-cover items-center pb-12  h-full flex flex-col  justify-end rounded-2xl`}
-        id={isActive ? "print" : undefined}
+        className={`${darkSide ? " bg-cardArirangWhite" : "bg-cardArirangRed"} bg-cover items-center pb-14  h-full flex flex-col  justify-end rounded-2xl`}
         data-testid="title"
       >
         <span
-          className={` px-2 ml-2 font-extrabold uppercase text-[1rem] pr-10 h-[73px] z-50 top-52 left-2 translate-x-[-2%] max-w-[200px] overflow-hidden text-ellipsis truncate whitespace-nowrap`}
+          className={` px-4 ml-2 font-extrabold uppercase text-[1rem] pr-10 h-[73px] z-50 top-48 left-2 translate-x-[-2%] max-w-[200px] overflow-hidden text-ellipsis `}
         >
           {songName}
         </span>
