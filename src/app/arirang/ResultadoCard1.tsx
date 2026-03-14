68923497d667ab95fb876024ec0c8e8a1db43cdb
GlendyT <glendytuyuc@gmail.com>
2026-03-13 19:06:33 -0600

    fixing responsive design

--- src/app/arirang/ResultadoCard1.tsx
+++ src/app/arirang/ResultadoCard1.tsx
@@ -35,7 +35,7 @@ const ResultadoCard1 = ({
       />
 
       <div className="absolute inset-0 flex items-center justify-center text-center gap-5 px-10 text-sm max-md:text-xs pt-44 max-sm:pt-6">
-        <div className="relative w-full flex flex-col items-center justify-center mt-10   ">
+        <div className="relative w-full flex flex-col items-center justify-center mt-10 max-md:mt-5 max-sm:mt-52 ">
           <span className="text-[0.7rem] flex flex-col  max-md:text-xs max-sm:text-[0.6rem] text-black tracking-tighter  p-2 max-sm:p-1  ">
         <span
           className={` px-0 font-extrabold uppercase text-[1rem] pr-14 h-[58px] pt-1   overflow-hidden text-ellipsis tracking-tighter leading-none `}
