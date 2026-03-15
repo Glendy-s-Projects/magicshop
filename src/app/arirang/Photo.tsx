ea14eb441fd08ca0aad6206c133370e2005c8717
GlendyT <glendytuyuc@gmail.com>
2026-03-15 16:45:54 -0600

    finishing the pfp

--- src/app/arirang/Photo.tsx
+++ src/app/arirang/Photo.tsx
@@ -10,7 +10,7 @@ type PhotoProps = {
   activeTab: "card" | "pfp";
 };
 
-const Photo = ({ preview, handleFileChange, activeTab }: PhotoProps) => {
+const Photo = ({ preview, handleFileChange }: PhotoProps) => {
   const { changeColor } = usePhotobooth();
   return (
     <div data-test-id="Photo" className="flex flex-col items-center m-1">
